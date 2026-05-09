b9a77e37d397248df2e87eca4cc876ca92408408
Reddy-labs <sravanthireddy9963@gmail.com>
2026-05-09 13:22:50 +0530

    Remove sanity

--- sanity/client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from 'next-sanity';
-
-export const client = createClient({
-  projectId: 'paste-your-real-id-here', // <-- e.g. "a1b2c3d4"
-  dataset: 'production',
-  apiVersion: '2024-05-09',
-  useCdn: false,
-});
